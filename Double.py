6be7fe4faa420acdf15f95987ea8ef99a852520e
DavidFitoussi <52169384+DavidFitoussi@users.noreply.github.com>
2019-09-16 20:20:01 +0300

    Add files via upload

--- Double.py
+++ Double.py
@@ -4,44 +4,46 @@ import random
 
 # Direction Enum used to check the different direction of line winner
 class Picture(Enum):
-    dubble = 0
-    lips = 1
+    dobble = 0
+    clown = 1
     yinyang = 2
-    dog = 3
+    babybottle = 3
     cat = 4
     Scissors = 5
     flower = 6
     leaf = 7
     dragon = 8
-    dolphin = 9
+    clock = 9
     drop = 10
-    IceCube = 11
+    hammer = 11
     igloo = 12
     ghost = 13
     bomb = 14
-    lock =15
+    stain =15
     musicnote = 16
     spider = 17
-    Cobwebs = 18
+    fire = 18
     Carrot = 19
-    tree = 20
-    lune = 21
+    body = 20
+    skelton = 21
     sun = 22
     NoEntry = 23
     Turtle = 24
     crayon = 25
     exclamationsign = 26
     questionsign = 27
-    heart = 28
+    leafred= 28
     cheese = 29
     Bird = 30
     Cible = 31
     Glasses = 32
-    Key = 33
+    cactus = 33
     Eye =34
     Clock = 35
     lamp = 36
+    flange = 37
     Car = 38
+    freeze = 39
 
 class clsGame:
     # constructor of Game , initialize the local variables
--- ServerSide.py
+++ ServerSide.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api, Resource , reqparse
 from flask_cors import CORS
 from Double import clsGame
+import json
 
 app  = Flask(__name__)
 CORS(app)
@@ -16,12 +17,8 @@ class Service (Resource):
     def GetCard():
         InstanceResource = clsGame()
         InstanceResource.InitMatrix()
-        result = str(InstanceResource.Card)
+        result = json.dumps(InstanceResource.Card)
         return result,200
-
-
-
-
 
 api.add_resource(Service)
 
